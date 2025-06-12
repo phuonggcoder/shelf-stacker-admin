6bb5bb96eeead8c5a941adf5a2ee4c9d54fc45ec
havivan1 <havivan2k5@gmail.com>
2025-06-12 11:44:43 +0700

    van
    update

--- app.js
+++ app.js
@@ -5,21 +5,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
-app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
-app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
-app.get('/detail', (req, res) => res.sendFile(path.join(__dirname, 'views', 'detail.html')));
-
-app.get('/danhgia', (req, res) => res.sendFile(path.join(__dirname, 'views', 'danhgia.html')));
-
-app.get('/danhgiasanpham', (req, res) =>
-  res.sendFile(path.join(__dirname, 'views', 'danhgiasanpham.html'))
-);
-app.get('/quanlynguoidung', (req, res) => res.sendFile(path.join(__dirname, 'views', 'quanlynguoidung.html')));
-app.get('/quanlydanhmuc', (req, res) => res.sendFile(path.join(__dirname, 'views', 'quanlydanhmuc.html')));
-app.get('/danhmucdonhang', (req, res) => res.sendFile(path.join(__dirname, 'views', 'danhmucdonhang.html')));
-
+app.get('/home.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/quanlydanhmuc.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'quanlydanhmuc.html')));
+app.get('/danhgiasanpham.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'danhgiasanpham.html')));
+app.get('/quanlynguoidung.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'quanlynguoidung.html')));
+app.get('/danhgia.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'danhgia.html')));
+app.get('/danhmucdonhang.html', (req, res) => res.sendFile(path.join(__dirname, 'views', 'danhmucdonhang.html')));
 
 const PORT = 3000;
 app.listen(PORT, () => {
